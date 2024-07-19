eaf5fafa08ae7a1a056dc0a08d574a130636e521
Learner062022 <ddylan246@yahoo.com>
2024-07-19 14:23:47 +1000

    Create project

---
+++ DylanDeSouzaExerciseTracker/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DylanDeSouzaExerciseTracker
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
